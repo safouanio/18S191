bb2e02e8986cf2cc7b2e5304152055d81caff1a9
Safouane <safouanio@users.noreply.github.com>
2020-09-30 23:26:32 +0100

    Update 01-images-intro.jl

--- lecture_notebooks/week1/01-images-intro.jl
+++ lecture_notebooks/week1/01-images-intro.jl
@@ -162,20 +162,15 @@ md"""## If in doubt: Ask Julia!
 """
 
 # ╔═╡ de373816-ec79-11ea-2772-ebdca52246ac
-begin
-	import Pkg
-	Pkg.activate(mktempdir())
-end
+
 
 # ╔═╡ 552129ae-ebca-11ea-1fa1-3f9fa00a2601
 begin
-	Pkg.add(["Images", "ImageIO", "ImageMagick"])
 	using Images
 end
 
 # ╔═╡ fbe11200-e938-11ea-12e9-6125c1b56b25
 begin
-	Pkg.add("PlutoUI")
 	using PlutoUI
 end
 
